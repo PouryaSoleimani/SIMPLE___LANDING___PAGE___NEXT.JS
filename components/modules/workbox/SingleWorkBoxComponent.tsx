6da6d2e137e30f4943d4671d75833a3b34b84d0e
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 23:58:58 +0330

    🟦 MUI 🟦

--- components/modules/workbox/SingleWorkBoxComponent.tsx
+++ components/modules/workbox/SingleWorkBoxComponent.tsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import Image from 'next/image';
 
 
 interface SingleWorkBoxProps { id: number, title: string, desc: string }
 
-const SingleWorkBoxComponent: React.FC<SingleWorkBoxProps> = ({ title, desc }) => {
+const SingleWorkBoxComponent: React.FC<SingleWorkBoxProps> = ({ id, title, desc }) => {
     return (
-        <div className='flex flex-col gap-y-6 mt- items-center justify-start content-start'>
-            <h3 className='text-2xl tracking-tight'>{title}</h3>
-            <p className='w-[60%] text-center text-lg text-zinc-600'>{desc}</p>
-        </div>
+        <Card sx={{ maxWidth: 345 }} >
+            <CardActionArea>
+                <Image src={`/work-${id}.jpg`} width={500} height={300} alt='work-image' />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div" className='text-center font-black'>{title}</Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }} style={{ fontSize: "16px", textAlign: "center" }}>
+                        {desc}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
     )
 }
 
