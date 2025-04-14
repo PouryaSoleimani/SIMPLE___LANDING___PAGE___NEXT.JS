f48b3d3db61cb20871536e409b7deccca259fddd
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 17:28:05 +0330

    🟦 MUI 🟦

--- components/modules/appbar/AppBarComponent.tsx
+++ components/modules/appbar/AppBarComponent.tsx
@@ -6,13 +6,14 @@ import { Button } from "@mui/material";
 function AppBarComponent() {
     return (
         <nav className='w-full h-auto flex items-center justify-between pr-3 pl-2 py-2 sticky top-3 left-0 bg-transparent rounded backdrop-blur-xl border border-slate-200'>
-            <div className="flex items-center justify-start basis-1/4">
+
+            <div className="flex items-center justify-start basis-1/2 md:basis-1/5">
                 <Link href="/">
                     <DiJqueryUiLogo className="w-16 h-16" />
                 </Link>
             </div>
 
-            <ul id="NAVIGATION____LINKS" className="flex items-center justify-evenly basis-2/4">
+            <ul id="NAVIGATION____LINKS" className="hidden lg:flex items-center justify-center gap-4 basis-3/5">
                 <Button variant="text" color="inherit"><Link href="/">Home</Link></Button>
                 <Button variant="text" color="inherit"><Link href="/">About</Link></Button>
                 <Button variant="text" color="inherit"><Link href="/">How it works</Link></Button>
@@ -20,7 +21,11 @@ function AppBarComponent() {
                 <Button variant="text" color="inherit"><Link href="/">Portfolio</Link></Button>
             </ul>
 
-            <div className="basis-1/4 flex items-center justify-end">
+
+            <div className="basis-1/2 items-center justify-end flex md:hidden">
+                <Button variant="contained" color="primary" size="medium">Contact Us</Button>
+            </div>
+            <div className="basis-1/5 items-center justify-end hidden md:flex">
                 <Button variant="contained" color="primary" size="large">Contact Us</Button>
             </div>
 
