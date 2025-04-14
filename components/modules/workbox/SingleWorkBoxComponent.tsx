97e42ddb1774d215bcb2cc7f2821e55a8c31ec8b
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 23:49:42 +0330

    🟦 MUI 🟦

--- components/modules/workbox/SingleWorkBoxComponent.tsx
+++ components/modules/workbox/SingleWorkBoxComponent.tsx
@@ -5,8 +5,8 @@ interface SingleWorkBoxProps { id: number, title: string, desc: string }
 
 const SingleWorkBoxComponent: React.FC<SingleWorkBoxProps> = ({ title, desc }) => {
     return (
-        <div className='flex flex-col items-center justify-start content-start'>
-            <h3>{title}</h3>
+        <div className='flex flex-col gap-y-6 mt- items-center justify-start content-start'>
+            <h3 className='text-2xl tracking-tight'>{title}</h3>
             <p className='w-[60%] text-center text-lg text-zinc-600'>{desc}</p>
         </div>
     )
