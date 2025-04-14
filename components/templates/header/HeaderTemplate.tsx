4709265309f75872e09c8bdafb8e6ac2e64d8324
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-14 22:09:57 +0330

    🟦 MUI 🟦

--- components/templates/header/HeaderTemplate.tsx
+++ components/templates/header/HeaderTemplate.tsx
@@ -6,7 +6,7 @@ function HeaderTemplate() {
     return (
         <>
             <AppBarComponent />
-            <BottomHeaderComponent/>
+            <BottomHeaderComponent />
         </>
     )
 }
