6eaf02716ff22aac6750ca2d82ed4f9536bb50ee
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-15 13:10:07 +0330

    🟦 MUI 🟦

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,24 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import SpeedDialComponent from "@/components/modules/speed-dial/SpeedDialComponent";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light', 
+  },
+});
+import CssBaseline from '@mui/material/CssBaseline';
+
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
-      <SpeedDialComponent />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+        <SpeedDialComponent />
+      </ThemeProvider>
     </>
   )
 }
