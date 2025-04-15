933b5bae224fe6d0c1728b7caac9ddb8d42f1285
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-16 02:17:32 +0330

    🟦 MUI 🟦

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,26 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import SpeedDialComponent from "@/components/modules/speed-dial/SpeedDialComponent";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
-import createCache from '@emotion/cache';
-
-const clientSideEmotionCache = createCache({ key: 'css', prepend: true });
-interface MyAppProps extends AppProps {
-  emotionCache?: typeof clientSideEmotionCache;
-}
-
-export default function App({ Component, pageProps, emotionCache = clientSideEmotionCache }: MyAppProps) {
-  const theme = createTheme({ palette: { mode: 'light', }, });
+import createEmotionCache from '../utils/emotionCache';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
 
 
+
+const clientSideEmotionCache = createEmotionCache();
+const theme = createTheme();
+
+
+
+function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <CacheProvider value={emotionCache}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Component {...pageProps} />
-          <SpeedDialComponent />
-        </ThemeProvider>
-      </CacheProvider>
-
-    </>
+    <CacheProvider value={clientSideEmotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+        <SpeedDialComponent />
+      </ThemeProvider >
+    </CacheProvider>
   )
 }
+export default MyApp;
---
+++ pages/text.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@mui/material'
+import React from 'react'
+
+function text() {
+    return (
+        <div>
+            <Button variant='contained'>CLICK ME</Button>
+        </div>
+    )
+}
+
+export default text
