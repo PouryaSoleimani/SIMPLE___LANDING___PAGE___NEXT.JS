ea4cd28de4564c740cbb6e5122a5416efcef1892
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-16 02:52:19 +0330

    🟦 MUI 🟦

--- components/modules/gallery-box/GalleryBoxComponent.tsx
+++ components/modules/gallery-box/GalleryBoxComponent.tsx
@@ -1,15 +1,13 @@
-import { Card, CardContent } from '@mui/material'
+import { Card } from '@mui/material'
 import Image from 'next/image'
 import React from 'react'
 
-interface GalleryBoxProps { src: string }
+interface GalleryBoxProps { id: number, src: string }
 
-const GalleryBoxComponent: React.FC<GalleryBoxProps> = ({ src }) => {
+const GalleryBoxComponent: React.FC<GalleryBoxProps> = ({ id, src }) => {
     return (
         <Card>
-            <CardContent>
-                <Image src={src} width={1024} height={768} alt='gallery-image' className='w-auto h-auto' />
-            </CardContent>
+            <Image src={src} width={1024} height={768} alt={`gallery-image-${id}`} className='h-full object-cover' />
         </Card>
     )
 }
--- components/templates/gallery/GalleryTemplate.tsx
+++ components/templates/gallery/GalleryTemplate.tsx
@@ -1,12 +1,27 @@
+import GalleryBoxComponent from '@/components/modules/gallery-box/GalleryBoxComponent'
 import React from 'react'
 
-
+const GalleryImages = [
+    { id: 1, src: "/design-1.png" },
+    { id: 1, src: "/design-2.jpg" },
+    { id: 1, src: "/design-3.jpg" },
+    { id: 1, src: "/design-4.jpg" },
+    { id: 1, src: "/design-5.webp" },
+    { id: 1, src: "/design-6.webp" },
+]
+type SingleGalleryImage = { id: number, src: string }
 
 function GalleryTemplate() {
     return (
         <div>
             <h2 className='text-center text-xl text-zinc-600 mt-8'>PORTFOLIO</h2>
             <h3 className='text-center my-2 text-4xl font-semibold tracking-wide'>Three Simple Step to Start Working Progress</h3>
+
+            <div className='flex'>
+                {GalleryImages.map((image: SingleGalleryImage) => (
+                    <GalleryBoxComponent key={image.id}  {...image} />
+                ))}
+            </div>
         </div>
     )
 }
