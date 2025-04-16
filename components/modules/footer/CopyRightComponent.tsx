f90cbb2e2f6526352c4d734f70df6b61a56304ef
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-16 11:56:42 +0330

    🟦 MUI 🟦

--- components/modules/footer/CopyRightComponent.tsx
+++ components/modules/footer/CopyRightComponent.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const CopyRightComponent = () => {
     return (
-        <div className='bg-white h-1/4'>CopyRightComponent</div>
+        <div className='bg-white h-1/4 flex items-center justify-between px-8 text-zinc-500 text-sm'>
+            <h3>Copy Right © 2024-2025</h3>
+            <h4>Interactive ART company . All Rights Reserved</h4>
+        </div>
     )
 }
 
--- components/modules/footer/UpperFooter.tsx
+++ components/modules/footer/UpperFooter.tsx
@@ -7,11 +7,11 @@ const UpperFooter = () => {
     return (
         <div className='h-full flex flex-col lg:flex-row items-center justify-evenly px-6'>
             <h2 className="text-white text-3xl font-bold">Create a new generation website for your business</h2>
-            <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250, borderRadius: "8px" }}  >
+            <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 250, borderRadius: "6px" }}  >
                 <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Email" inputProps={{ 'aria-label': 'Email' }} />
                 <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
                 <IconButton color="primary" sx={{ p: '5px' }} aria-label="directions">
-                    <BsFillArrowRightSquareFill className="w-8 h-8 rounded-lg" />
+                    <BsFillArrowRightSquareFill className="w-8 h-8 rounded-md" />
                 </IconButton>
             </Paper>
         </div>
