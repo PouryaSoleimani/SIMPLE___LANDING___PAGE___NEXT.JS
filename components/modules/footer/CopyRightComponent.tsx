895c3f28b9b749603e4c4dd327e64b8785e0ece3
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-16 11:51:59 +0330

    🟦 MUI 🟦

--- components/modules/footer/CopyRightComponent.tsx
+++ components/modules/footer/CopyRightComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CopyRightComponent = () => {
     return (
-        <div className='bg-white h-1/3'>CopyRightComponent</div>
+        <div className='bg-white h-1/4'>CopyRightComponent</div>
     )
 }
 
--- components/modules/footer/UpperFooter.tsx
+++ components/modules/footer/UpperFooter.tsx
@@ -1,8 +1,20 @@
+import { BsFillArrowRightSquareFill } from "react-icons/bs";
+
+import { Divider, IconButton, InputBase, Paper } from '@mui/material'
 import React from 'react'
 
 const UpperFooter = () => {
     return (
-        <div className='h-2/3'>UpperFooter</div>
+        <div className='h-full flex flex-col lg:flex-row items-center justify-between px-6'>
+            <h2>Create a new generation website for your business</h2>
+            <Paper component="form" sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width: 400 }}  >
+                <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Email" inputProps={{ 'aria-label': 'Email' }} />
+                <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+                <IconButton color="primary" sx={{ p: '10px' }} aria-label="directions">
+                    <BsFillArrowRightSquareFill />
+                </IconButton>
+            </Paper>
+        </div>
     )
 }
 
