3d07a1d52a6c8016a4a1773d1c22e95ac6602731
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-16 12:11:18 +0330

    🟦 MUI 🟦

--- components/templates/footer/FooterTemplate.tsx
+++ components/templates/footer/FooterTemplate.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const FooterTemplate = () => {
     return (
-        <footer className='w-full pt-4 h-64  bg-blue-600 mt-10'>
+        <footer className='w-full pt-4 h-64  bg-blue-600 mt-20'>
             <UpperFooter />
             <CopyRightComponent />
         </footer>
