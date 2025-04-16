bdc55b4973630b4694af77154700278344d169e0
PouryaSoleimani <pouryasoleimani93@gmail.com>
2025-04-16 12:53:29 +0330

    🟦 MUI 🟦

--- components/modules/appbar/AppBarComponent.tsx
+++ components/modules/appbar/AppBarComponent.tsx
@@ -2,11 +2,16 @@ import { ImMenu } from "react-icons/im";
 import { DiJqueryUiLogo } from "react-icons/di";
 import React, { useState } from 'react'
 import Link from "next/link";
-import { Button } from "@mui/material";
+import { Button, Divider } from "@mui/material";
 
 
 function AppBarComponent() {
     const [isMenuShow, setIsMenuShow] = useState(false)
+
+    function menuHandler() {
+        setIsMenuShow(prev => !prev)
+    }
+
     return (
         <>
             <nav className='w-full h-auto flex items-center justify-between pr-3 pl-2 py-2 sticky top-2 left-0 bg-white/50 rounded backdrop-blur-2xl border border-slate-200 z-50'>
@@ -27,7 +32,7 @@ function AppBarComponent() {
 
 
                 <div className="basis-1/2 items-center justify-end flex md:hidden">
-                    <ImMenu className="w-10 h-10 backdrop-blur-3xl border-zinc-500 p-1 rounded" />
+                    <ImMenu className="w-10 h-10 backdrop-blur-3xl border-zinc-500 p-1 rounded" onClick={menuHandler} />
                 </div>
                 <div className="basis-1/5 items-center justify-end hidden md:flex">
                     <Button variant="contained" color="primary" size="large">Contact Us</Button>
@@ -35,12 +40,14 @@ function AppBarComponent() {
 
             </nav>
 
-            <ul id="NAVIGATION____LINKS" className="flex md:hidden flex-col pt-16 items-start text-start fixed left-0 top-0 bg-blue-600 text-white  h-screen w-[60vw] z-50">
+            <ul id="NAVIGATION____LINKS" className={`flex md:hidden flex-col pt-6 items-start text-start duration-300 fixed ${!isMenuShow ? "left-[-250px]" : "left-0"} top-0 bg-blue-600 text-white  h-screen w-[60vw] z-50`}>
+                <DiJqueryUiLogo className="w-16 h-16 ml-2" />
                 <Button variant="text" color="inherit" sx={{ textAlign: "start", width: "100%" }}><Link href="/" className="text-xl my-4 w-full px-2">Home</Link></Button>
                 <Button variant="text" color="inherit" sx={{ textAlign: "start", width: "100%" }}><Link href="/" className="text-xl my-4 w-full px-2">About</Link></Button>
                 <Button variant="text" color="inherit" sx={{ textAlign: "start", width: "100%" }}><Link href="/" className="text-xl my-4 w-full px-2">How it works</Link></Button>
                 <Button variant="text" color="inherit" sx={{ textAlign: "start", width: "100%" }}><Link href="/" className="text-xl my-4 w-full px-2">Clients</Link></Button>
                 <Button variant="text" color="inherit" sx={{ textAlign: "start", width: "100%" }}><Link href="/" className="text-xl my-4 w-full px-2">Portfolio</Link></Button>
+                <Divider variant="inset" color="white" sx={{ height: "2px" }} />
             </ul>
 
         </>
